e6b8aeae9a904c46d123621c2b7d39439dd46944
Javier Ortega <jvaierortega@gmail.com>
2026-06-21 11:34:09 +0200

    Cigarette in process

---
+++ src/main/java/com/alteredstates/client/ModItemProperties.java
@@ -0,0 +1,25 @@
+package com.alteredstates.client;
+
+import com.alteredstates.AlteredStates;
+import com.alteredstates.item.CigaretteItem;
+import com.alteredstates.registry.ModItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ModItemProperties {
+    public static void register(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ItemProperties.register(
+                    ModItems.CIGARETTE.get(),
+                    ResourceLocation.fromNamespaceAndPath(AlteredStates.MOD_ID, "visual_state"),
+                    (stack, level, entity, seed) -> {
+                        if (stack.getItem() instanceof CigaretteItem cigarette) {
+                            return cigarette.getVisualStateIndex(stack);
+                        }
+                        return 0.0F;
+                    }
+            );
+        });
+    }
+}
